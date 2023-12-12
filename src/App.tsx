867e0871b0a0286791afb940caad47982ffcb9f0
aleksandr_skidan@rntgroup.com <aleksandr_skidan@rntgroup.com>
2023-12-12 19:34:44 +0300

    create react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
+import Product from "./components/product";
+
+interface Post {
+    id: number,
+    title: string,
+    body: string,
+    userId: number,
+}
 
 function App() {
+    const [posts, setPosts] = useState([]);
+
+    useMemo(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((json) => {
+                setPosts(json)
+            });
+    }, []);
+
     return (
-        <div className="App">
-            Hello !!!
+        <div className="container mx-auto max-w-2xl pt-5">
+            <Product/>
         </div>
     );
+
 }
 
 export default App;
